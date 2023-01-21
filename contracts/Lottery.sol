330ae2aa4ff2d1d65aad35bf726e71f8952dfa22
emanherawy <eman.herawy@gmail.com>
2023-01-21 22:31:09 +0200

    Solution for Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,30 +9,36 @@ contract Lottery {
     address[] public gameWinners;
     address public owner;
 
+modifier onlyOwner() {
+require(msg.sender==owner,"ONLY_OWNER");
+    _;
+}
+
     // declaring the constructor
     constructor() {
+        owner=msg.sender;
         // TODO: initialize the owner to the address that deploys the contract
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
+        require(msg.value==0.1 ether);
+        players.push(msg.sender);
         // TODO: require each player to send exactly 0.1 ETH
         // TODO: append the new player to the players array
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view onlyOwner returns (uint256) {
         // TODO: restrict this function so only the owner is allowed to call it
         // TODO: return the balance of this address
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
+    function pickWinner() public  onlyOwner{
         // TODO: only the owner can pick a winner 
         // TODO: owner can only pick a winner if there are at least 3 players in the lottery
-
-        uint256 r = random();
-        address winner;
 
         // TODO: compute an unsafe random index of the array and assign it to the winner variable 
 
@@ -41,6 +47,13 @@ contract Lottery {
         // TODO: reset the lottery for the next round
 
         // TODO: transfer the entire contract's balance to the winner
+        require(players.length>=3,"NOT_ENOUGH_PLAYERS");
+        uint256 winnerIndex = random()%players.length;
+        address winner = players[winnerIndex];
+        gameWinners.push(winner);
+        delete players;
+        payable(winner).transfer(address(this).balance);
+
     }
 
     // helper function that returns a big random integer
